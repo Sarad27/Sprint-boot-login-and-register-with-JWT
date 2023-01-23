8668ac1a9ad5b8f99ad2846fd844c46670c5a05c
Sarad <sarad.subedi@kotuko.it>
2023-01-23 16:08:38 +0545

    Added validation error handling

--- src/main/java/com/sarad/facialrecognizationattendance/exception/ControllerExceptionHandler.java
+++ src/main/java/com/sarad/facialrecognizationattendance/exception/ControllerExceptionHandler.java
@@ -2,11 +2,15 @@ package com.sarad.facialrecognizationattendance.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -33,6 +37,26 @@ public class ControllerExceptionHandler {
                 .build();
 
         return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationMessage> handleValidationException(MethodArgumentNotValidException ex, WebRequest request){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        var validationMessage = ValidationMessage.builder()
+                .message(errors)
+                .description("Validation Error : " + request.getDescription(false))
+                .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .timestamp(new Date())
+                .build();
+
+        return new ResponseEntity<ValidationMessage>(validationMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+
     }
 
 }
